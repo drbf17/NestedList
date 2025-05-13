c029b7adbe2af68ecde5bee89186aa296215262a
Dalmo Rangel Braga Filho <dalmo.filho@caixa.gov.br>
2025-05-13 02:01:06 -0300

    feat: Controle de scroll

--- app/src/main/java/br/com/drbf/nestedlist/MainActivity.kt
+++ app/src/main/java/br/com/drbf/nestedlist/MainActivity.kt
@@ -6,23 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.drbf.nestedlist.ui.MyNestedListScreen
+import br.com.drbf.nestedlist.ui.NestedListScreen
 import br.com.drbf.nestedlist.ui.Repos
 import br.com.drbf.nestedlist.ui.theme.NestedListTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NestedListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyNestedListScreen(
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text("My App Title")
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    NestedListScreen(
                         modifier = Modifier.padding(innerPadding),
                         menus = Repos.menus,
                     )
--- app/src/main/java/br/com/drbf/nestedlist/ui/List.kt
+++ app/src/main/java/br/com/drbf/nestedlist/ui/List.kt
@@ -7,14 +7,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,26 +29,45 @@ fun NestedListScreen(
     menus: List<Menu>,
 ) {
 
+
     var myMenus by remember { mutableStateOf(emptyList<Menu>()) }
 
     LaunchedEffect(menus) {
         myMenus = menus
     }
+    val listState = rememberLazyListState()
+    var lastClickedIndex by remember { mutableIntStateOf(-1) }
+
     val onClick = remember {
         { menu: Menu ->
-            myMenus = myMenus.map {
-                if (it == menu) {
-                    it.copy(isExpanded = !it.isExpanded)
+            myMenus = myMenus.map { tempMenu ->
+                if (tempMenu == menu) {
+                    lastClickedIndex = myMenus.indexOfFirst { it == menu } + 5
+                    tempMenu.copy(isExpanded = !tempMenu.isExpanded)
                 } else {
-                    it
+                    tempMenu
                 }
             }
         }
     }
 
+
+    LaunchedEffect(listState) {
+        snapshotFlow { listState.layoutInfo.totalItemsCount }
+            .collect { _ ->
+                if (listState.layoutInfo.visibleItemsInfo.last().index == lastClickedIndex) {
+                    listState.scrollToItem(lastClickedIndex)
+                    lastClickedIndex = -1
+                }
+            }
+    }
+
+
+
     NestedList(
         modifier = modifier,
         menus = myMenus,
+        listState = listState,
         onClick = onClick
     )
 }
@@ -54,16 +77,20 @@ fun NestedListScreen(
 fun NestedList(
     modifier: Modifier = Modifier,
     menus: List<Menu>,
-    onClick: (menu: Menu) -> Unit = {}
-) {
+    listState: LazyListState,
+    onClick: (menu: Menu) -> Unit = {},
+
+    ) {
+
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.background)
-            .padding(vertical = 8.dp)
+            .padding(vertical = 8.dp),
+        state = listState
     ) {
 
-        item {
+        items(5) {
             Text(
                 text = "Nested List",
                 modifier = Modifier
